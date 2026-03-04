bbcdb77b147be95c2d25426ea9ab83b9cc4069dc
Likhit Chirmade <likhitchirmade@gmail.com>
2026-03-04 21:10:31 +0530

    added support for \n

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
 void kernel_main(void) {
+	gdt_init();
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	printf("My name is Likhit");
 }
